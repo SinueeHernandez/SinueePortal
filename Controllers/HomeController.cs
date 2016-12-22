bbbf87b53a902682da61180cc2d70a10f5dd5cb6
Sinuee Hernandez <sinuee@gmail.com>
2016-12-22 10:27:45 -0600

    Updating home views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace WebApplication.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sinuee Hernandez - Developer.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "You need a hand - just tell me.";
 
             return View();
         }
