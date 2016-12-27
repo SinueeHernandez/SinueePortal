edfe8bf66e993089ae844a1fbef0a75d339f1c27
Sinuee Hernandez <sinuee@gmail.com>
2016-12-27 12:37:21 -0600

    Post creation and index

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -1,26 +1,45 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication.Data;
+using WebApplication.Models;
+using System.Linq;
 
 namespace WebApplication.Controllers
 {
     public class Blog : Controller
     {
+        private readonly ApplicationDbContext _DbContext;
 
-        //
+        public Blog (ApplicationDbContext DbContext)
+        {
+            _DbContext = DbContext;
+        }
+        
         // GET: /Account/Login
         [HttpGet]
         [AllowAnonymous]
         public IActionResult Index(string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
-            return View();
+            return View(_DbContext.Posts.ToList());
         }
 
         public IActionResult NewPost()
         {
-            ViewData["Title"] = "New Post creation";
-            ViewData["Message"] = "Create a new post here";
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult NewPost(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _DbContext.Posts.Add(post);
+                _DbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(post);
         }
     }
 }
