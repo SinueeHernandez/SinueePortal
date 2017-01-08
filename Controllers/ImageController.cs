2c72c195f2135630dd18409045938af80e28b548
Sinuee Hernandez <sinuee@gmail.com>
2017-01-08 01:12:35 -0600

    images showing

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -21,12 +21,20 @@ namespace WebApplication.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymousAttribute]
-        public FileStreamResult GetImage(int ImageId)
+        [AllowAnonymous]
+        public IActionResult GetImage(int ImageId)
         {
             var imageResult = _DbContext.Images.ToList().Where(p => p.Id == ImageId).FirstOrDefault();
-            Stream stream = new MemoryStream(imageResult.Data);
-            return new FileStreamResult(stream, imageResult.Type);
+            return View(imageResult);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public FileStreamResult GetImageForDisplay (int ImageId)
+        {
+            var imageResult = _DbContext.Images.ToList().Where(p => p.Id == ImageId).FirstOrDefault();
+            Stream fileResult = new MemoryStream(imageResult.Data, false);
+            return new FileStreamResult(fileResult,imageResult.Type);
         }
 
         public IActionResult Add ()
@@ -42,15 +50,11 @@ namespace WebApplication.Controllers
             {
                 if (file != null && file.Length > 0)
                 {
-                    var uploads = Path.Combine(_environment.WebRootPath, "uploads");
-                    using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
-                    {
-                        file.CopyToAsync(fileStream);
-                        image.Data = new byte[fileStream.Length];
-                        fileStream.Read(image.Data,0,image.Data.Length);
-                        image.Name = file.FileName;
-                        image.Type = file.ContentType;
-                    }
+                    MemoryStream ms = new MemoryStream();
+                    file.CopyTo(ms);
+                    image.Data = ms.ToArray();
+                    image.Name = file.FileName;
+                    image.Type = file.ContentType;
                 }
                 
                 _DbContext.Images.Add(image);
@@ -58,6 +62,20 @@ namespace WebApplication.Controllers
                 return View(image);
             }
             return View(image);
+        }
+
+        public static byte[] ReadFully(Stream input)
+        {
+            byte[] buffer = new byte[16*1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
         }
     }
 }
